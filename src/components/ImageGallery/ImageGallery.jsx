c1108063baad9db910a75d5ef5522f0ed382b775
Igor <115535399+IgorVinnik85@users.noreply.github.com>
2023-05-10 17:59:08 +0300

    update01

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,12 +1,51 @@
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-import React from 'react';
-import css from './ImageGallery.module.css'
+import React, { Component } from 'react';
+import css from './ImageGallery.module.css';
+import { Button } from 'components/Button/Button';
+
+const KEY = '34813361-3927ac478a2bf3f204ffaaf5a';
+export class ImageGallery extends Component {
+  state = {
+    arrayImages: [],
+    page: 1,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevProps.images !== this.props.images ||
+      prevState.page !== this.state.page
+    ) {
+      fetch(
+        `https://pixabay.com/api/?q=${this.props.images}&page=${this.state.page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
+      )
+        .then(res => res.json())
+        .then(res => {
+          this.setState(prevState => {
+            return {
+              arrayImages: [...prevState.arrayImages, ...res.hits],
+            };
+          });
+        });
+    }
+  }
 
 
-export const ImageGallery = ({ images }) => {
-  return (
-    <ul className={css.imageGallery}>
-      <ImageGalleryItem arrayImages={images} />
-    </ul>
-  );
-};
+  btnAddImages = () => {
+    this.setState(prevState => {
+      return {
+        page: prevState.page + 1,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <ul className={css.imageGallery}>
+          <ImageGalleryItem arrayImages={this.state.arrayImages} />
+        </ul>
+        {this.props.images && <Button addImages={this.btnAddImages} />}
+      </>
+    );
+  }
+}
