ce6cf5d9a270cf6c55190c63619c3919c83a77f9
Igor <115535399+IgorVinnik85@users.noreply.github.com>
2023-05-12 15:12:58 +0300

    portal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import css from './Modal.module.css';
 import PropTypes from 'prop-types';
+import { createPortal } from 'react-dom';
 
+const modalRoot = document.querySelector('#modal-root');
 export class Modal extends Component {
   state = {};
 
@@ -22,16 +24,16 @@ export class Modal extends Component {
 
   render() {
     const { modalClose, alt, url } = this.props;
-    return (
+    return createPortal(
       <div className={css.overlay} onClick={modalClose}>
         <div className={css.modal}>
           <img src={url} alt={alt} />
         </div>
-      </div>
+      </div>,
+      modalRoot
     );
   }
 }
-
 
 Modal.propTypes = {
   modalClose: PropTypes.func.isRequired,
